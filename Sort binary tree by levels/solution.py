192568077ad78e9523ee918cedbafbce14cdf878
Sofia Trush <trush.pn@ucu.edu.ua>
2025-05-02 12:58:39 +0300

    Add code solution for Sort binary tree by levels

--- Sort binary tree by levels/solution.py
+++ Sort binary tree by levels/solution.py
@@ -1,1 +1,19 @@
+"""Sort binary tree by levels"""
 
+def tree_by_levels(node):
+    """main function which sort binary tree by levels"""
+    if not node:
+        return []
+
+    result = []
+    queue = [node]
+
+    while queue:
+        node = queue.pop(0)
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+        result.append(node.value)
+
+    return result
